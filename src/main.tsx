77fba0ca0e3c169a5dcce2948788b48d1f3e46cd
Minh Lam <lamworkspace74@gmail.com>
2026-04-24 15:57:03 +0700

    added speed insight

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,12 @@ import { BrowserRouter } from 'react-router-dom'
 import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import App from './App.tsx'
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <SpeedInsights />
       <Analytics />
     </BrowserRouter>
   </StrictMode>,
